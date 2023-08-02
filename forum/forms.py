a73fd8b8d6fb88ecff0c256f749412ebba47747a
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-08-02 10:47:53 +0300

    add samarnote from content in form

--- forum/forms.py
+++ forum/forms.py
@@ -1,8 +1,9 @@
 
 
 from django import forms
-
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 from .models import Question,Answers
+
 
 
 
@@ -13,6 +14,10 @@ class QuestionForm(forms.ModelForm):
         model = Question
         # fields ='__all__'
         exclude =('Author',)
+        widgets = {
+            'content': SummernoteWidget(),
+          
+        }
 
 
 
--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'forum',
     'taggit',
     'bootstrap5',
+    'django_summernote', 
 ]
 
 MIDDLEWARE = [
