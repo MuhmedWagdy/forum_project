a2c5a564574b5fab33a253adccabed172dd36131
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-08-13 14:44:10 +0300

    add choices field in class text choise

--- forum/models.py
+++ forum/models.py
@@ -10,13 +10,20 @@ from taggit.managers import TaggableManager
 
 
 
-
 class Question(models.Model):
+
+    class Status(models.TextChoices):
+        Draft = 'DF' ,'Draft'
+        Puplished = 'PB' ,' Puplished'
+        
     Author=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
     question = models.CharField(max_length=200)
     tags = TaggableManager()
     created_at = models.DateField(default=timezone.now)
     Content = models.TextField(max_length=20000)
+    status = models.CharField(max_length=5,choices=Status.choices,default=Status.Draft)
+
+
 
     def __str__(self):
         return self.question
